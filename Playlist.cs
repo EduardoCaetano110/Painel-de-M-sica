44a3b953905f2900ab76ba59f946cb3a47a53181
Caetano <119038092+EduardoCaetano110@users.noreply.github.com>
2023-11-09 12:23:51 -0300

    Add files via upload

---
+++ Playlist.cs
@@ -0,0 +1,21 @@
+﻿class Playlist
+{
+    public Playlist(string nomep, string ordemp)
+    {
+        Nomep = nomep;
+        Ordemp = ordemp;
+    }
+
+    public string Nomep { get; }
+    public string Ordemp { get; }
+
+    public void AddMusic (Musica musica) 
+    {
+        
+    }
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Banda djonga = new Banda("Djonga");
+
+Album albumDoDjonga = new Album("Nu");
+
+Musica musica1 = new Musica(djonga, "Nós")
+{
+    Duracao = 314,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(djonga, "Ó Quem Chega")
+{
+    Duracao = 333,
+    Disponivel = false,
+
+};
+
+albumDoDjonga.AdicionarMusica(musica1);
+albumDoDjonga.AdicionarMusica(musica2);
+djonga.AdicionarAlbum(albumDoDjonga);
+
+musica1.ExibirFichaTenica();
+musica2.ExibirFichaTenica();
+
+albumDoDjonga.ExibirMusicasDoAlbum();
+djonga.ExibirDiscografia();
+
